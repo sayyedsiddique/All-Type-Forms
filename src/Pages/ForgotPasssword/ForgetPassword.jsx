b232d698404e24e6d9ce962f639c143770c69863
sayyedsiddique <sayyedsiddique055@gmail.com>
2023-05-29 13:12:15 +0530

    forgot password page functionality completed

--- src/Pages/ForgotPasssword/ForgetPassword.jsx
+++ src/Pages/ForgotPasssword/ForgetPassword.jsx
@@ -2,6 +2,7 @@ import { AccountCircle } from "@material-ui/icons";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import {
+  Alert,
   Box,
   Button,
   FormControl,
@@ -9,38 +10,113 @@ import {
   Input,
   InputAdornment,
   InputLabel,
+  Snackbar,
   TextField,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { HiOutlineDevicePhoneMobile } from "react-icons/hi2";
 import { HiOutlineMail } from "react-icons/hi";
 import PhoneInput from "react-phone-input-2";
 import { useNavigate } from "react-router-dom";
 import CardWithTwoSection from "../../Components/CardBox/CardWithTwoSection";
 import { BiArrowBack } from "react-icons/bi";
+import { LoadingButton } from "@mui/lab";
 // import ForgetPass1 from '../../assets/images/forgetpass1.jpg'
 
 const ForgetPassword = () => {
   const navigate = useNavigate();
-  const [showPassword, seShowPassword] = useState(false);
+  const emailInputRef = useRef(null);
+  const mobilenputRef = useRef(null);
   const [isEmail, setIsEmail] = useState(true);
+  const [openPopUp, setOpenPopUp] = useState(false);
+  const [btnLoading, setBtnLoading] = useState(false);
 
   const [fields, setFields] = useState({
     email: "",
     phone: "",
-    password: "",
-    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    phone: "",
   });
 
   //   Register with Email or Mobile handle
   const registerWithHandler = () => {
+    setErrors({ ...errors, email: "", phone: "" });
+    setFields({ ...fields, email: "", phone: "" });
     setIsEmail(!isEmail);
   };
+
+    // Popup alert close handler
+    const popUpCloseHandler = () => {
+      setOpenPopUp(false);
+    };
+  
 
   //   Sign up handler navigate to registration page
   const signUpHandler = () => {
     navigate("/registration");
   };
+
+  // forgot password form input fields handle
+  const inputHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log("value ", value);
+
+    if (name === "email") {
+      setErrors({ ...errors, email: "" });
+      setFields({ ...fields, email: value });
+    } else if (name === "phone") {
+      setErrors({ ...errors, phone: "" });
+      setFields({ ...fields, phone: value });
+    }
+  };
+
+    // forgot password form validation check handle
+    const validation = () => {
+      // Perform email validation
+      const regexEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+      const isEmailValid = regexEmail.test(fields.email);
+  
+      if (isEmail === true && fields.email === "") {
+        setErrors({ ...errors, email: "Please enter your email" });
+        if (emailInputRef.current) {
+          emailInputRef.current.focus();
+        }
+        return false;
+      } else if (isEmail === true && isEmailValid === false) {
+        setErrors({ ...errors, email: "Please enter valid email" });
+        if (emailInputRef.current) {
+          emailInputRef.current.focus();
+        }
+        return false;
+      } else if (isEmail === false && fields.phone === "") {
+        setErrors({ ...errors, phone: "Please enter your phone" });
+        if (mobilenputRef.current) {
+          mobilenputRef.current.focus();
+        }
+        return false;
+      }
+  
+      return true;
+    };
+
+    const submitHandler = () => {
+      const val = validation();
+  
+      if (val) {
+        setOpenPopUp(true);
+        setBtnLoading(true);
+  
+        setTimeout(() => {
+          // setOpenPopUp(true);
+          setBtnLoading(false);
+          navigate("/forgot-password-verification");
+        }, 3000);
+      }
+    };
 
   return (
     <CardWithTwoSection>
@@ -74,13 +150,14 @@ const ForgetPassword = () => {
               placeholder={"Enter your email"}
               id="outlined-size-small"
               size="small"
-              name="productName"
-              // inputRef={prodNameInputRef}
-              // onChange={productInputHanlder}
+              name="email"
+              inputRef={emailInputRef}
+              onChange={inputHandler}
+              value={fields.email}
             />
-            {/* {error && error?.productName && (
-                <span className="text-danger">{error?.productName}</span>
-              )} */}
+            {errors && errors?.email && (
+              <span className="text-danger">{errors?.email}</span>
+            )}
           </div>
         ) : (
           <div className="input_container mb-3">
@@ -93,36 +170,20 @@ const ForgetPassword = () => {
               placeholder={"Enter your mobile no"}
               id="outlined-size-small"
               size="small"
-              name="mobile"
-              // inputRef={prodNameInputRef}
-              // onChange={productInputHanlder}
+              name="phone"
+              inputRef={mobilenputRef}
+              type={"number"}
+              inputProps={{ maxLength: 7 }}
+              onChange={inputHandler}
+              value={fields.phone}
             />
-            {/* {error && error?.productName && (
-              <span className="text-danger">{error?.productName}</span>
-            )} */}
+              {errors && errors?.phone && (
+                <span className="text-danger">{errors?.phone}</span>
+              )}
           </div>
         )}
-        {/* <div className="input_container mb-3">
-            <InputLabel style={{ color: "var(--product-text-color)" }}>
-              Password
-              <span className="text-danger">*</span>
-            </InputLabel>
-            <TextField
-              style={{ backgroundColor: "var( --light-gray-color)" }}
-              placeholder={"Enter your password"}
-              id="outlined-size-small"
-              size="small"
-              name="password"
-              inputRef={prodNameInputRef}
-              onChange={productInputHanlder}
-            />
-          </div> */}
-
-        {/* <div className="forget_password text-right">
-            <p className="heading_para">Forget password?</p>
-          </div> */}
         <div className="mt-2" style={{ textAlign: "right" }}>
-          <Button
+          {/* <Button
             variant="contained"
             style={{
               backgroundColor: "var(--button-bg-color)",
@@ -134,12 +195,22 @@ const ForgetPassword = () => {
             // onClick={submitHandler}
           >
             Submit
-          </Button>
-          {/* <div className="orDIvider">
-              <hr />
-              <p>OR</p>
-              <hr />
-            </div> */}
+          </Button> */}
+          <LoadingButton
+              loading={btnLoading}
+              loadingPosition="start"
+              variant="contained"
+              style={{
+                backgroundColor: "var(--button-bg-color)",
+                color: "var(--button-color)",
+                width: "100%",
+                fontSize: "1.1rem",
+                letterSpacing: "1px",
+              }}
+              onClick={submitHandler}
+            >
+              Sign in
+            </LoadingButton>
           <div className="login_container" style={{ marginTop: "2rem" }}>
             <p
               className="heading_para"
@@ -150,6 +221,20 @@ const ForgetPassword = () => {
             </p>
           </div>
         </div>
+        <Snackbar
+            open={openPopUp}
+            autoHideDuration={3000}
+            onClose={popUpCloseHandler}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          >
+            <Alert
+              onClose={popUpCloseHandler}
+              severity="success"
+              sx={{ width: "100%" }}
+            >
+              Verification code sent to your email.
+            </Alert>
+          </Snackbar>
       </div>
     </CardWithTwoSection>
   );
