4a90805a7b3f2e6303a2db80d9bc52b208861e23
sayyedsiddique <sayyedsiddique055@gmail.com>
2023-05-29 13:02:08 +0530

    home page created

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="home_page_container">
+      <div className="home_page_inner">
+        <h1>Welcome to the home page.</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
